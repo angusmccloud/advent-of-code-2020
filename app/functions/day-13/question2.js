02230167a8c44015db540dcce2cf21c07910a8a2
Connor Tyrrell <connor@brewgene.com>
2020-12-13 20:01:21 -0500

    Day 13 code cleanup

--- app/functions/day-13/question2.js
+++ app/functions/day-13/question2.js
@@ -13,17 +13,12 @@ const question2 = (inputData = []) => {
     }
 
     busses.sort((a, b) => b.busId - a.busId);
-    // console.log(busses);
+    console.log(busses);
 
-    let result = {result1: 0, result2: 0, diff: busses[0].busId};
+    let result = {result1: -busses[0].offset, result2: 0, diff: busses[0].busId};
 
     for(let i = 0; i < busses.length - 1; i++) {
-        let offset = 0;
-        if(i === 0){
-            offset = busses[0].offset;
-        }
-        
-        result = findMatch(result.result1, result.diff, offset, busses[i+1].busId, busses[i+1].offset);
+        result = findMatch(result.result1, result.diff, busses[i+1].busId, busses[i+1].offset);
         // console.log(result);
     }
 
@@ -32,32 +27,26 @@ const question2 = (inputData = []) => {
 
 module.exports = question2; 
 
-const findMatch = (startNum, increment, masterOffset, num, offset) => {
-    let result1 = 0;
-    let result2 = 0;
-    let i = startNum - masterOffset;
+const findMatch = (startNum, increment, num, offset) => {
+    let result = 0;
+    let i = startNum;
     let target = offset;
     while(target > num) {
         target = target - num;
     }
 
-    while(result2 === 0) {
+    while(result === 0) {
         const possibleTs = i;
         const remainder = possibleTs % num;
         if(((remainder === 0 && target === 0) || ((num - target) === remainder)) && possibleTs > increment) {
-            if(result1 === 0){
-                result1 = possibleTs;
-            } else {
-                result2 = possibleTs;
-            }
+            result = possibleTs;
         }
 
         i = i + increment;
     }
 
     return {
-        result1: result1,
-        result2: result2,
-        diff: result2 - result1
+        result1: result,
+        diff: num * increment
     };
 }
